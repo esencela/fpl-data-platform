da8023f79b20577b87f6c10cdab03edcb6493e1b
Alec Woods <alecwoods@aol.com>
2026-04-10 15:14:05 +0100

    Add extract function for vaastav fixture data

--- src/vaastav_extract.py
+++ src/vaastav_extract.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pathlib import Path
 from datetime import datetime
+from urllib.error import HTTPError
 import logging
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
@@ -12,13 +13,16 @@ MAX_SEASON = 2025
 
 logger = logging.getLogger(__name__)
 
+
 def get_season_string(season: int) -> str:
     """Converts a season integer (e.g. 2026) to a string format used in file paths (e.g. '2025-26')."""
     start_year = season - 1
 
     return f'{start_year}-{str(season)[-2:]}'
 
+
 seasons = [(get_season_string(season), season) for season in range(MIN_SEASON, MAX_SEASON + 1)] # Extraction of FPL API data starts from 25/26 season, we only need historic data up to 24/25 season
+base_url = f'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/'
 
 
 def extract_player_data() -> None:
@@ -27,18 +31,23 @@ def extract_player_data() -> None:
     logger.info('Extracting player data from GitHub...')  
 
     for season_string, season in seasons:
-        base_url = f'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/{season_string}/'
-
         base_path = RAW_DATA_DIR / 'players' / f'season={season}'
 
         # Raw player data
-        player_url = base_url + 'players_raw.csv'
-        df_player = pd.read_csv(player_url)
+        player_url = base_url + f'{season_string}/players_raw.csv'
 
-        file_path = base_path / f'{CURRENT_DATE}.parquet'
-        file_path.parent.mkdir(parents=True, exist_ok=True)
-        df_player.to_parquet(file_path, index=False)
-        logger.info(f'Player data for season {season} extracted and saved to {file_path}')
+        try:
+            df_player = pd.read_csv(player_url)
+
+            file_path = base_path / f'{CURRENT_DATE}.parquet'
+            file_path.parent.mkdir(parents=True, exist_ok=True)
+            df_player.to_parquet(file_path, index=False)
+
+            logger.info(f'Player data for season {season} extracted and saved to {file_path}')
+        
+        except Exception as e:
+            logger.error(f'Failed to retrieve player data: {e}')
+            raise
 
 
 def extract_gameweek_data() -> None:
@@ -47,15 +56,50 @@ def extract_gameweek_data() -> None:
     logger.info('Extracting player-game data from GitHub...')  
 
     for season_string, season in seasons:
-        base_url = f'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/{season_string}/gws/'
-
         base_path = RAW_DATA_DIR / 'gws' / f'season={season}'
 
         # Raw player-game data
-        gw_url = base_url + 'merged_gw.csv'
-        df_gw = pd.read_csv(gw_url, encoding='latin-1') # utf-8 fails so use latin-1 for github data
+        gw_url = base_url + f'{season_string}/gws/merged_gw.csv'
 
-        file_path = base_path / f'{CURRENT_DATE}.parquet'
-        file_path.parent.mkdir(parents=True, exist_ok=True)
-        df_gw.to_parquet(file_path, index=False)
-        logger.info(f'Player-Game data for season {season} extracted and saved to {file_path}')
+        try:
+            df_gw = pd.read_csv(gw_url, encoding='latin-1') # utf-8 fails so use latin-1 for github data
+
+            file_path = base_path / f'{CURRENT_DATE}.parquet'
+            file_path.parent.mkdir(parents=True, exist_ok=True)
+            df_gw.to_parquet(file_path, index=False)
+
+            logger.info(f'Player-Game data for season {season} extracted and saved to {file_path}')
+
+        except Exception as e:
+            logger.error(f'Failed to retrieve player-game data: {e}')
+            raise
+
+def extract_fixture_data() -> None:
+    """Extracts raw fixture CSV files from GitHub and saves as parquet files"""
+
+    logger.info('Extracting fixture data from GitHub...')
+
+    for season_string, season in seasons:
+        base_path = RAW_DATA_DIR / 'fixtures' / f'season={season}'
+
+        # Raw fixture data
+        fixture_url = base_url + f'{season_string}/fixtures.csv'
+
+        try:
+            df_fixtures = pd.read_csv(fixture_url)
+
+            file_path = base_path / f'{CURRENT_DATE}.parquet'
+            file_path.parent.mkdir(parents=True, exist_ok=True)
+            df_fixtures.to_parquet(file_path, index=False)
+
+            logger.info(f'Fixture data for season {season} extracted and saved to {file_path}')
+
+        except HTTPError as e: 
+            if e.code == 404:                
+                logger.warning(f'Fixture data not available for {season} season')
+            else:
+                logger.error(f'Failed to retrieve fixture data: {e}')
+                raise
+        except Exception as e:
+            logger.error(f'Failed to retrieve fixture data: {e}')
+            raise
