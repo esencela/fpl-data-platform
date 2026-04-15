07eebb1d7dfe64ece1cd48eb305a26c941be7aa7
Alec Woods <alecwoods@aol.com>
2026-04-15 16:36:42 +0100

    Add intermediate model for team-season
    - Also update missing_teams seed file to include missing strength metrics

---
+++ dbt/models/intermediate/int_team_season.sql
@@ -0,0 +1,59 @@
+{{ config(
+    materialized='incremental',
+    incremental_strategy='delete+insert',
+    unique_key=['team_season_key']
+) }}
+
+with combined as (
+    select 
+        *,
+        'fpl' as source
+    from {{ ref('stg_fpl__teams') }}
+    union all
+    select
+        *,
+        'vaastav' as source
+    from {{ ref('stg_vaastav__teams') }}
+    union all
+    select
+        *,
+        'inferred' as source
+    from {{ ref('missing_teams') }}
+),
+
+-- If rows clash, prioritise by source
+prioritised as (
+    select
+        *,
+        row_number() over(
+            partition by season, team_season_id
+            order by case
+                when source = 'fpl' then 1
+                when source = 'vaastav' then 2
+                else 3
+            end
+        ) as r
+    from combined
+)
+
+select 
+    -- Team identifiers
+    concat(season, '_', team_season_id) as team_season_key,
+    season,
+    team_id,
+    team_season_id,
+
+    -- Team names
+    name,
+    short_name,
+
+    -- Team strength metrics
+    strength_overall_home,
+    strength_overall_away,
+    strength_attack_home,
+    strength_attack_away,
+    strength_defence_home,
+    strength_defence_away
+
+from prioritised
+where r = 1
