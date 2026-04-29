04c8fd61e296f06e593fa0c163cc04c5d8d58ff5
Alec Woods <alecwoods@aol.com>
2026-04-29 16:32:01 +0100

    Add date key to fact_player_game

--- dbt/models/serving/fact_player_game.sql
+++ dbt/models/serving/fact_player_game.sql
@@ -10,6 +10,7 @@ select
         else fixture.away_team_id
     end as team_id,
     player_game.fixture_key,
+    extract(date from fixture.kickoff_time) as date_key,
 
     -- Game details
     case -- Some players have zero minutes but have still played
