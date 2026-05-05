8797f88d2d8f50a04f160661c079f94870d8a369
Alec Woods <alecwoods@aol.com>
2026-05-05 18:03:25 +0100

    Add staging model for understat player data

---
+++ dbt/models/staging/stg_understat__players.sql
@@ -0,0 +1,35 @@
+{{ config(
+    alias='understat_players',
+    materialized='view'
+) }}
+
+-- Only get data from most recent fetch
+with latest_snapshot as (
+    select
+        raw_data,
+        season
+    from {{ source('raw', 'understat_season_data')}}
+    where fetched_at = (select max(fetched_at) from {{ source('raw', 'understat_season_data') }})
+),
+
+source_data as (
+    select
+        season,
+        jsonb_array_elements(raw_data->'players') as player_data
+    from latest_snapshot
+),
+
+players as (
+    select
+        season::int as season,
+        (player_data->>'id')::int as player_id,
+        replace(player_data->>'player_name', '&#039;', '''') as player_name -- Replace html escape for apostrophe
+    
+    from source_data
+)
+
+select
+    distinct player_id,
+    player_name
+    
+from players
