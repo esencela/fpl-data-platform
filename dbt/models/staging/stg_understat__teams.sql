86631064f2e6940378d621537ca3bbd030055889
Alec Woods <alecwoods@aol.com>
2026-05-05 18:35:20 +0100

    Add team codes to understat team staging model from fixtures data

--- dbt/models/staging/stg_understat__teams.sql
+++ dbt/models/staging/stg_understat__teams.sql
@@ -18,10 +18,34 @@ source_data as (
         value as team_data
     from latest_snapshot,
     jsonb_each(raw_data->'teams')
+),
+
+-- Team endpoint data is missing three letter team codes, so we need to get this from the fixture data
+fixture_data as (
+    select
+        season,
+        jsonb_array_elements(raw_data->'dates') as fixture_data
+    from latest_snapshot
+),
+
+fixture_team_data as (
+    select
+        distinct (fixture_data->'h'->>'id')::int as team_id,
+        (fixture_data->'h'->>'short_title') as short_name
+    from fixture_data
+),
+
+team_data as (
+    select
+        distinct team_id::int as team_id,
+        (team_data->>'title') as team_name
+    from source_data
 )
 
 select
-    distinct team_id::int as team_id,
-    (team_data->>'title') as team_name
-
-from source_data
+    t.team_id,
+    t.team_name,
+    f.short_name
+from team_data t
+left join fixture_team_data f
+    on t.team_id = f.team_id
