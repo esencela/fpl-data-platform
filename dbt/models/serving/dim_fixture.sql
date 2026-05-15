aa651a6ed744d367da6fcd225800e6785861189c
Alec Woods <alecwoods@aol.com>
2026-05-15 16:34:22 +0100

    Add understat data to fixture dimension table

--- dbt/models/serving/dim_fixture.sql
+++ dbt/models/serving/dim_fixture.sql
@@ -1,15 +1,23 @@
 {{ config(materialized='table') }}
 
 select
+	-- Identifiers
 	fixture.fixture_key,
-	fixture.season,
+	fixture.fpl_fixture_id,
 	fixture.fpl_fixture_season_id,
+	fixture.understat_fixture_id,
+
+	-- Fixture info
+	fixture.season,
 	fixture.gameweek_id,
 	fixture.gameweek as canon_gameweek,
 	fixture.kickoff_time,
 	fixture.finished,
+
+	-- Team identifiers
 	home.team_id as home_team_id,
 	away.team_id as away_team_id
+	
 from {{ ref('int_fixtures') }} as fixture
 join {{ ref('int_team_season') }} as home
 	on fixture.home_team_season_key = home.team_season_key
