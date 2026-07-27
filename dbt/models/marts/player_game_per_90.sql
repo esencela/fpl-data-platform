8f266695c1d950611977a5fcd1bbf94e99240479
Alec Woods <alecwoods@aol.com>
2026-07-27 13:47:34 +0100

    Create per 90 data table in marts schema
    - Aggregates selected stat columns per 90 minutes of gametime
    - Will be used as features for machine learning

---
+++ dbt/models/marts/player_game_per_90.sql
@@ -0,0 +1,40 @@
+{{ config(materialized='table') }}
+
+{% set stat_columns = [
+    'goals', 
+    'assists', 
+    'goal_involvements', 
+    'shots', 
+    'key_passes', 
+    'goals_conceded', 
+    'own_goals', 
+    'yellow_cards', 
+    'red_cards', 
+    'defensive_contributions', 
+    'saves', 
+    'bps', 
+    'influence', 
+    'creativity', 
+    'ict_index', 
+    'expected_goals', 
+    'expected_assists', 
+    'expected_goal_involvements', 
+    'expected_goal_chain', 
+    'expected_goal_buildup'
+] %}
+
+select
+    player_game_key,
+
+    {% for stat in stat_columns %}
+    SUM({{ stat }}) over w / nullif(sum(minutes) over w, 0)::numeric * 90 as {{stat}}_per90_todate{{ "," if not loop.last}}
+    {% endfor %}
+
+from {{ ref('fact_player_game') }} pg
+join {{ ref('dim_fixture') }} f
+on pg.fixture_key = f.fixture_key
+window w as (
+    partition by player_id, season
+    order by date_key
+    rows between unbounded preceding and current row
+)
