e3c79b3d3b16f7acabbfdfd08d093c3b1b14acfa
Alec Woods <alecwoods@aol.com>
2026-09-24 17:16:15 +0100

    Add python script that takes features and outputs model predictions

--- src/inference/features.py
+++ src/inference/features.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sqlalchemy import create_engine, text
 
 def get_prediction_features(db_url: str, schema: str, table: str) -> pd.DataFrame:
-    """Reads prediction features from database and returns a pandas DataFrame"""
+    """Reads prediction features from database and returns a pandas DataFrame."""
 
     engine = create_engine(db_url)
 
---
+++ src/inference/predict.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from inference.model_store import ModelMeta
+
+REQUIRED_ID_COLUMNS = ['fixture_key', 'player_id']
+
+def predict(features: pd.DataFrame, model, meta: ModelMeta) -> pd.DataFrame:
+    """Takes features DataFrame and returns model predictions."""
+
+    missing_ids = [col for col in REQUIRED_ID_COLUMNS if col not in features.columns]
+
+    if missing_ids:
+        raise ValueError(f'Feature table is missing column(s): {missing_ids}')
+
+    missing_features = set(meta.feature_columns) - set(features.columns)
+
+    if missing_features:
+        raise ValueError(f'Feature table is missing feature(s): {missing_features}')
+
+    X = features[meta.feature_columns]
+    yhat = model.predict(X)
+
+    df = features[REQUIRED_ID_COLUMNS].copy()
+    df['target'] = meta.target
+    df['predicted_value'] = yhat
+    df['model_name'] = meta.model_name
+    df['model_version'] = meta.model_version
+
+    return df
